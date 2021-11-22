473e097c1e993f154877d4b33408727645871102
Shashank-Panda <shashankpanda54@gmail.com>
2021-11-22 11:49:55 +0530

    minor setup changes

--- voting_dapp/settings.py
+++ voting_dapp/settings.py
@@ -15,7 +15,9 @@ import os
 import dotenv
 
 from django.conf import settings
+import pymysql
 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
